5449108dcbde0805fce1f34d87b6512617dc5f1f
eogml0404 <eogml0404@naver.com>
2024-07-05 08:53:17 +0900

    0704

---
+++ 0704/0704/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _0704
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Box1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // Box1
+            // 
+            this.Box1.Font = new System.Drawing.Font("Consolas", 30F);
+            this.Box1.Location = new System.Drawing.Point(-1, 3);
+            this.Box1.Margin = new System.Windows.Forms.Padding(7, 9, 7, 9);
+            this.Box1.Multiline = true;
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(631, 509);
+            this.Box1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 507);
+            this.Controls.Add(this.Box1);
+            this.Font = new System.Drawing.Font("Consolas", 20F);
+            this.Margin = new System.Windows.Forms.Padding(7, 9, 7, 9);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Box1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ 0704/0704/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0704
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        
+            int cnt = 0;
+            while (cnt < 5)
+            {
+
+                if (cnt == 0)
+                {
+                    Box1.Text += "Loop Start! \r\n";
+                    cnt++;
+                }
+                else
+                    Box1.Text += cnt++.ToString() + "\r\n";
+
+            }
+
+        }
+
+
+    }
+}
