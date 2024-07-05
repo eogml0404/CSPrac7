daad18e605c92a76942109f7804c12d60770d87c
eogml0404 <eogml0404@naver.com>
2024-07-05 13:51:08 +0900

    0705

---
+++ 0705/0705/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace _0705
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Box1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Box1
+            // 
+            this.Box1.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box1.Location = new System.Drawing.Point(154, 59);
+            this.Box1.Multiline = true;
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(442, 319);
+            this.Box1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Box1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Box1;
+    }
+}
+
---
+++ 0705/0705/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0705
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //배열 초기화
+            int[] array1 = new int[5];
+
+            //배열 생성
+            int[] array2 = { 1, 2, 3, 4, 5, 6 };
+            
+            int[,] multiDimensionalArray1 = new int[2, 3];
+            int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            //불규칙 배열 - > 잘 안쓰긴함
+            int[][] jaggedArray = new int[6][];
+            jaggedArray[0] = new int[4] { 1, 2, 3, 4 };
+            jaggedArray[1] = new int[3] { 1, 2, 3 };
+
+            //for (int i = array2.Length-1; i >= 0 ; i--)
+              //  Box1.Text += array2[i].ToString() + " ";
+            
+           /* foreach(int j in multiDimensionalArray2)
+            {
+                Box1.Text += j.ToString() + " ";
+            }
+*/
+
+            //행의 길이 GetLength(0), 열의 길이 GetLength(1)
+            /*for(int j = 0; j < multiDimensionalArray2.GetLength(0); j++)
+            {
+                for(int k = 0; k < multiDimensionalArray2.GetLength(1); k++)
+                {
+                    Box1.Text += multiDimensionalArray2[j,k] + " ";
+                }
+            }*/
+
+            string codingon = "codingon";
+            string[] a = "1 2 3".Split(' ');
+            Box1.Text += a[0] + " ";
+            Box1.Text += a[1] + " ";
+            Box1.Text += a[2] + " ";
+
+            Box1.Text = codingon.Replace("on", "off");
+            Box1.Text = "";
+
+
+
+            string str = "string 5";
+            string[] strArr = str.Split(' ');
+
+            string str1 = strArr[0];
+            int num = int.Parse(strArr[1]);
+
+            for (int i = 0; i < num; i++)
+                Box1.Text += str1;
+
+
+
+
+
+        }
+            
+    }
+}
