63471f365c9370945301d93fcfb91e963f258d04
eogml0404 <eogml0404@naver.com>
2024-07-05 13:46:23 +0900

    Btn

---
+++ Btn/Btn/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Btn
+{
+    public partial class Form1 : Form
+    {
+        //Form1 의 생성자
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   
+        //버튼 클릭했을 때 실행
+        private void button_hello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕!");
+        }
+
+        private void Box1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
