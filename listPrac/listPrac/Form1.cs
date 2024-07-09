ac4ccf53053aa2772b845a6cd2a356e4cebc3a74
eogml0404 <eogml0404@naver.com>
2024-07-09 22:47:36 +0900

    주석 추가

---
+++ listPrac/listPrac/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listPrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<int> a = new List<int>();
+            a.Add(1);
+            a.Add(2);
+            a.Remove(1);
+            a.Contains(1);
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
